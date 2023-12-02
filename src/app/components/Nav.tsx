d361a0236bb92d32bb01198910e1d23528b24ea9
Amit Roy <amitroy497@yahoo.com>
2023-12-02 18:44:13 +0530

    Pending UI Footer and Navbar

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,35 +1,72 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
+import { CgCloseR, CgMenu } from 'react-icons/cg';
 
 import styles from '@/app/styles/navbar.module.css';
 
 const Nav = () => {
+	const [openMenu, setOpenMenu] = useState(false);
 	return (
-		<nav className={styles.navbar}>
-			<div className=''>
-				<ul className={styles.navbarList}>
-					<li className={styles.navbarItem}>
-						<Link className={styles.navbarLink} href='/'>
-							Home
-						</Link>
-					</li>
-					<li className={styles.navbarItem}>
-						<Link className={styles.navbarLink} href='/about'>
-							About
-						</Link>
-					</li>
-					<li className={styles.navbarItem}>
-						<Link className={styles.navbarLink} href='/movie'>
-							Movie
-						</Link>
-					</li>
-					<li className={styles.navbarItem}>
-						<Link className={styles.navbarLink} href='/contact'>
-							Contact
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
+		<>
+			<nav className={styles.navbar}>
+				<div className={openMenu ? `${styles.active}` : ''}>
+					<ul className={styles.navbarList}>
+						<li className={styles.navbarItem}>
+							<Link
+								className={styles.navbarLink}
+								href='#'
+								onClick={() => setOpenMenu(false)}
+							>
+								Home
+							</Link>
+						</li>
+						<li className={styles.navbarItem}>
+							<Link
+								className={styles.navbarLink}
+								href='/about'
+								onClick={() => setOpenMenu(false)}
+							>
+								About
+							</Link>
+						</li>
+						<li className={styles.navbarItem}>
+							<Link
+								className={styles.navbarLink}
+								onClick={() => setOpenMenu(false)}
+								href='/movie'
+							>
+								Movie
+							</Link>
+						</li>
+						<li className={styles.navbarItem}>
+							<Link
+								className={styles.navbarLink}
+								onClick={() => setOpenMenu(false)}
+								href='/contact'
+							>
+								Contact
+							</Link>
+						</li>
+					</ul>
+
+					{/* //nav icon */}
+					<div className={styles['mobile-navbar-btn']}>
+						<CgMenu
+							name='menu-outline'
+							className={styles['mobile-nav-icon']}
+							onClick={() => setOpenMenu(true)}
+						/>
+						<CgCloseR
+							name='close-outline'
+							className={`${styles['mobile-nav-icon']} ${styles['close-outline']}`}
+							onClick={() => setOpenMenu(false)}
+						/>
+					</div>
+				</div>
+			</nav>
+		</>
 	);
 };
 
