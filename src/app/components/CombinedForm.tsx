f1259f477570fe98fa98c2f47df133be1c226c85
Amit Roy <amitroy497@yahoo.com>
2023-12-02 19:18:17 +0530

    UI fixed

--- src/app/components/CombinedForm.tsx
+++ src/app/components/CombinedForm.tsx
@@ -11,20 +11,26 @@ const CombinedForm = () => {
 	const [serverSideDataBase, setServerSideDataBase] = useState<boolean>(false);
 	return (
 		<div className={styles.container}>
-			<button
-				disabled={serverSideDataBase ? false : true}
-				onClick={() => setServerSideDataBase(false)}
-				className={`${!serverSideDataBase && styles.btndisabled}`}
-			>
-				Client Side Post Operation
-			</button>
-			<button
-				disabled={serverSideDataBase ? true : false}
-				onClick={() => setServerSideDataBase(true)}
-				className={`${serverSideDataBase && styles.btndisabled}`}
-			>
-				Server Side Post Operation
-			</button>
+			<div>
+				<div className={styles.buttonSection}>
+					<button
+						disabled={serverSideDataBase ? false : true}
+						onClick={() => setServerSideDataBase(false)}
+						className={`${!serverSideDataBase && styles.btndisabled}`}
+					>
+						Client Side Post Operation
+					</button>
+				</div>
+				<div className={styles.buttonSection}>
+					<button
+						disabled={serverSideDataBase ? true : false}
+						onClick={() => setServerSideDataBase(true)}
+						className={`${serverSideDataBase && styles.btndisabled}`}
+					>
+						Server Side Post Operation
+					</button>
+				</div>
+			</div>
 			{serverSideDataBase ? <ContactFormWithServerActions /> : <ContactForm />}
 		</div>
 	);
